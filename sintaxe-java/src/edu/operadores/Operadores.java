025c340cd5119207dc1448f663dd22ce7a82c033
Anderson Junior <acfaj2010@gmail.com>
2023-08-25 15:31:52 -0300

    Aulas operadores e tipos variaveis

---
+++ sintaxe-java/src/edu/operadores/Operadores.java
@@ -0,0 +1,32 @@
+package edu.operadores;
+
+public class Operadores {
+  public static void main(String[] args) {
+    // operadores aritimeticos
+    // adicao +
+    // subtracao -
+    // multiplicacao *
+    // divisao /
+    // modulo %
+    // incremento ++
+    // decremento --
+
+    // operador ternario
+    // declaracao:
+    // int numero1 = 10;
+    // String ternario = numero1 > 5 ? "Verdadeiro" : "Falso"
+
+    // operadores relacionais
+    // igualdade ==
+    // diferença !=
+    // maior que >
+    // maior igual >=
+    // menor que <
+    // menor igual <=
+
+    // operadores logicos
+    // operador E &&
+    // operador OU ||
+    // operador negacao !
+  }
+}
---
+++ sintaxe-java/src/edu/tipos_variaveis/TiposVariaveis.java
@@ -0,0 +1,34 @@
+package edu.tipos_variaveis;
+
+public class TiposVariaveis {
+  public static void main(String[] args) {
+
+    // variaveis
+
+    // declaracao de float
+    float numero1 = 50.0F;
+    // declaracao de double
+    double numero2 = 50.0;
+
+    // declaracao de String
+    String palavra1 = "Palavra";
+    char letra1 = 'a';
+
+    // declaracao de numeros inteiros
+    byte numero3;
+    short numero4;
+    int numero5;
+    long numero6 = 125238534586L;
+
+    // boolean
+    boolean valor = true;
+
+    // variable casting
+    short numero7 = (short) numero6;
+
+    // constantes
+    // "final" para definir constantes, convenção é escrever o nome em uppercase
+    final String PALAVRA_2 = "Palavra";
+
+  }
+}
